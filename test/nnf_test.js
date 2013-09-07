9db44a02be94db93f8b57acab03b47daa112d3ac
divanvisagie <visagiedivan@gmail.com>
2013-09-08 00:54:02 +0200

    Started Implementation

--- test/nnf_test.js
+++ test/nnf_test.js
@@ -22,15 +22,37 @@ var nnf = require('../lib/nnf.js');
     test.ifError(value)
 */
 
-exports['awesome'] = {
-  setUp: function(done) {
+exports['Server'] = {
+  setUp: function( done ) {
     // setup here
+    this.nnfServer = nnf.createServer();
+    this.nnfServer.listen(8080);
+
+    this.proto = [
+      { name: 'listen' },
+      { name: 'close' }
+    ];
+    this.instance = [];
+
     done();
   },
-  'no args': function(test) {
-    test.expect(1);
-    // tests here
-    test.equal(nnf.awesome(), 'awesome', 'should be awesome.');
+  shape: function( test ){
+    test.expect( this.proto.length + this.instance.length );
+
+    this.proto.forEach(function( method ) {
+      test.equal( typeof this.nnfServer[ method.name ], 'function' );
+    }, this);
+
+    this.instance.forEach(function( property ) {
+      test.notEqual( typeof this.nnfServer[ property.name ], 'undefined' );
+    }, this);
+
     test.done();
+  },
+  tearDown: function( done ) {
+    
+    this.nnfServer.close();
+
+    done();
   },
 };
